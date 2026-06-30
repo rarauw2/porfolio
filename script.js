8f830a826b6e434163c5c7f5f46690124bf93ac4
rarauw2 <raul.arroyo3@alu.uclm.es>
2026-06-30 17:25:39 +0200

    dark mode

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Este código se ejecuta cuando la página carga
 console.log("¡Hola! El archivo JavaScript está funcionando correctamente.");
+const boton = document.getElementById('boton-oscuro');
+
+boton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
