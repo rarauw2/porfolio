f50dec3e37b47b3d9d912cde8b536c498bd1f966
rarauw2 <raul.arroyo3@alu.uclm.es>
2026-07-06 00:57:19 +0200

    refine comments

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Este código se ejecuta cuando la página carga
-console.log("¡Hola! El archivo JavaScript está funcionando correctamente.");
 const boton = document.getElementById('boton-oscuro');
 
 boton.addEventListener('click', () => {
